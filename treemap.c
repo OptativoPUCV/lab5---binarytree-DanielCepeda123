77accd6cee3af9cc53649cd13ebdbdfac520a775
Replit user <>
2023-04-20 14:59:59 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -64,7 +64,6 @@ TreeNode * minimum(TreeNode * x){
 
 void removeNode(TreeMap * tree, TreeNode* node) {
 
-  
   TreeNode *aux = node->parent;
   TreeNode *aux2;
 
@@ -75,12 +74,14 @@ void removeNode(TreeMap * tree, TreeNode* node) {
     }
     
     if(aux->left == node){
+      if(aux == tree->root) tree->root->left = NULL;
       aux2 = aux->left;
       aux->left = NULL;
       free(aux2);
     }
     else{
       if(aux->right == node){
+        if (aux == tree->root) tree->root->right = NULL;
         aux2 = aux->right;
         aux->right = NULL;
         free(aux2);
