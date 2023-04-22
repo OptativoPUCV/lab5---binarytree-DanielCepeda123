ffc6bbf04d40f16e01ac7ad14e20d2eac53acfbc
Replit user <>
2023-04-22 05:23:45 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -185,10 +185,11 @@ Pair * upperBound(TreeMap * tree, void* key) {
     if(tree->lower_than(tree->current->pair->key, key) == 1)
     {
       tree->current = tree->current->right;
-      return NULL;
+      if(tree->current->pair->key == key) return tree->current->pair;
       
     }else{
       tree->current = tree->current->left;
+      if(tree->current->pair->key == key) return tree->current->pair;
       
       
     }
